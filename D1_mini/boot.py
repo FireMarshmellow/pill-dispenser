421bd5fe048d42ab05bfd6fd7a9d1c2a635b1386
Mellow <62353091+FireMarshmellow@users.noreply.github.com>
2022-08-17 02:34:59 +0100

    main

---
+++ D1_mini/boot.py
@@ -0,0 +1,82 @@
+import network, re
+import time
+import config
+import servo_driver
+from machine import RTC
+import ntptime
+
+
+wlan = None
+
+# this connect to the Wi-Fi network to get accurate a time.
+def connectWifi(ssid, passwd):
+    global wlan
+    cnt = 0
+    print("Start Wifi.")
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    print("Connect Wifi.", end="")
+    wlan.disconnect()
+    wlan.connect(ssid, passwd)
+    while not wlan.isconnected():
+        time.sleep(0.5)
+        print(".", end="")
+    print(" OK")
+    print("IP ADDR:", wlan.ifconfig()[0])
+    return True
+
+
+connectWifi(config.ssid, config.password)
+
+# this gets the date and time from network Time protocol,
+# and filters it down to just hours and minutes.
+def Whats_the_time():
+    rtc = RTC()
+    ntptime.host = "pool.ntp.org"
+    ntptime.NTP_DELTA = 3155673600 - ((config.timezone + 1) * 3600)
+    Time_clean = (
+        re.sub("[(){}]", "", str(rtc.datetime()[4:6]))
+        .replace(",", ":")
+        .replace(" ", "")
+    )
+    return Time_clean
+
+
+# this goes through the list of times in in config,
+# and dispensers the pill at the right time.
+def check_time(time):
+    for i in config.contaner1:
+        if i == time:
+            print("contaner_1")
+            servo_driver.Contener(config.servo1pin)
+    for i in config.contaner2:
+        if i == time:
+            print("contaner_2")
+            servo_driver.Contener(config.servo2pin)
+    for i in config.contaner3:
+        if i == time:
+            print("contaner_3")
+            servo_driver.Contener(config.servo3pin)
+    for i in config.contaner4:
+        if i == time:
+            print("contaner_4")
+            servo_driver.Contener(config.servo4pin)
+
+
+# This test each servo individually,
+# note: the server will not stop unless the vibration sensor is triggered.
+def test_run():
+    servo_driver.Contener(config.servo1pin)
+    servo_driver.Contener(config.servo2pin)
+    servo_driver.Contener(config.servo3pin)
+    servo_driver.Contener(config.servo4pin)
+
+
+# This loop will check the list against the time every minute,
+# note: if it's set to less than a minute it will go through
+# the list twice with the same time dispensing more pills then required.
+while True:
+    # test_run()
+    # print(Whats_the_time())
+    check_time(Whats_the_time())
+    time.sleep(60)
---
+++ D1_mini/config.py
@@ -0,0 +1,23 @@
+# fill in your Wi-Fi name and password.
+ssid = ""
+password = ""
+
+# Change this if the time I'm out put is wrong.
+timezone = 0
+
+# please enter at what time you'd like which container to release pills.
+# this works on a 24-hour clock if you require two of the same pill please enter the time twice.
+# note if it is a single digit time do not put a 0 in front of it, for example 9:05 a.m. correct:(9:5) wrong:(09:05).
+# if you wanted to using the container just leave the list empty.
+contaner1 = ["17:30", "17:30", "22:0"]
+contaner2 = ["9:0", "4:0"]
+contaner3 = ["9:0"]
+contaner4 = []
+
+# fill in the GPIO pins numbers for the servos and vibration sensor.
+senserpin = 14  # D5 = GPIO 14
+
+servo1pin = 5  # D1 = GPIO 5
+servo2pin = 4  # D2 = GPIO 4
+servo3pin = 12  # D6 = GPIO 12
+servo4pin = 15  # D8 = GPIO 15
